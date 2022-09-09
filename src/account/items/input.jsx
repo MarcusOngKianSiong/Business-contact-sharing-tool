a23ed58e16814ac15a03af2b70d82c60ae47ae41
Marcus <marcusongkiansiong@gmail.com>
2022-09-09 12:20:29 +0800

    something

--- src/account/items/input.jsx
+++ src/account/items/input.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React,{useState} from "react";
 
-export default function input(){
+
+export default function Input(prop){
+    const valueName = prop.valueName;
+    const reference = prop.reference;
+    const [value,setValue] = useState("");
+    
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+    
     return(
-        <input type="text"></input>
+        
+        <div>
+            <label for={valueName}>{valueName}</label>
+            <input type="text" id={valueName} ref={reference} value={value} onChange={(event)=>handleChange(event)}/>
+        </div>
+        
     )
 }
 
--- src/account/profile.jsx
+++ src/account/profile.jsx
@@ -1,4 +1,7 @@
+import { toHaveErrorMessage } from '@testing-library/jest-dom/dist/matchers';
+import { keyboardImplementationWrapper } from '@testing-library/user-event/dist/keyboard';
 import React,{useState,useEffect,useRef} from 'react';
+import InputValues from './items/input'
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,41 +13,67 @@ import {
   useHistory
 } from "react-router-dom";
 
+
+
 export default function Profile(prop){
 
-    // replace with prop.[]
-    const name = "bob"
-    const contact = 12345
-    const email = "bob@gmail.com"
+    const [currentSavedValues, setCurrentSavedValues] = useState({
+        profileImage: prop.profileImage,
+        name: prop.name,
+        email: prop.email,
+        contact: prop.contact,
+        editOrSave: "edit"
+    })
 
-    const [editOrSave,setEditOrSave] = useState({
-        next: "save",
-        editPersonalDetails: ""
-    });
-
-    const changeEditToSaveAndViceVersa = () => {
-        if(editOrSave.next === "edit"){
-            setEditOrSave({next: "save",editPersonalDetails: ""})
-        }else{
-            setEditOrSave({next: "edit", editPersonalDetails: "disabled"})
-        }
+    const checkValuesAndSave = () => {
+        console.log()
     }
 
-    useEffect(()=>{
+    const inputEmailReference = useRef();
+    const inputNameReference = useRef();
+    const inputContactReference = useRef();
+    
+    const handleSubmit = async () => {
 
-    },[editOrSave])
+
+        if(currentSavedValues.editOrSave === "save"){
+            await fetch(`http://localhost:3000/save?email=${inputEmailReference}&name=${inputNameReference}&contact=${inputContactReference}`)
+            .then((res)=>{
+                return res.json();
+            })
+            .then((res)=>{
+                if(res.DataSavedSuccessfully === true){
+                    // Change save to edit
+                }
+            
+        
+                if(res.DataSavedSuccessfully === false){
+                    // Add a new message in the form
+                
+                }
+            })
+        }
+        
+        if(currentSavedValues === "edit"){
+            setCurrentSavedValues((current)=>{
+                current.editOrSave = "save";
+                return current;
+            })
+            // create a new button
+        }
+
+    }
 
     return(
         <form>
             <img id="profileImage" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png" alt="Lalalala"/>
-            <label for="name">Name</label>
-            <input type="text" value="siometh" id="name"/>
-            <label for="contact">Contact</label>
-            <input type="text" value={contact} disabled={editOrSave.editPersonalDetails} id="contact"/>
-            <label for="email">Email</label>
-            <input type="text" value={email} disabled={editOrSave.editPersonalDetails} id="email"/>
-            <input type="button" value={editOrSave.next} onClick={changeEditToSaveAndViceVersa}/>
+            <InputValues valueName="name" reference={inputNameReference}/>
+            <InputValues valueName="Contact" reference={inputContactReference}/>
+            <InputValues valueName="email" reference={inputEmailReference}/>
+            <input type="button" value={currentSavedValues.editOrSave} onClick={handleSubmit}/>
         </form>
     )
 }
+
+
 
